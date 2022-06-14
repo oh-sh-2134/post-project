a1398b68bb0144acf0f501dff1c90a4b708b72dc
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-06-15 01:17:17 +0900

    Spring Security

---
+++ src/main/java/spring/postproject/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package spring.postproject.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    // 정적 자원에 대해서는 Security 설정을 적용하지 않음.
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                    // /about 요청에 대해서는 로그인을 요구함
+                    .antMatchers("/about").authenticated()
+                    // /admin 요청에 대해서는 ROLE_ADMIN 역할을 가지고 있어야 함
+                    .antMatchers("/admin").hasRole("ADMIN")
+                    // 나머지 요청에 대해서는 로그인을 요구하지 않음
+                    .anyRequest().permitAll()
+                    .and()
+                // 로그인하는 경우에 대해 설정함
+                .formLogin()
+                    // 로그인 페이지를 제공하는 URL을 설정함
+                    .loginPage("/member/loginForm")
+                    // 로그인 성공 URL을 설정함
+                    .successForwardUrl("/post/home")
+                    // 로그인 실패 URL을 설정함
+                    .failureForwardUrl("/member/loginForm")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
