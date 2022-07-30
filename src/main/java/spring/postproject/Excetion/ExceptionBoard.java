a5779f27a93cc8e2b793dd4b0b6c79867dec9969
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-07-30 18:41:34 +0900

    comment save, delete api

--- src/main/java/spring/postproject/Excetion/ExceptionBoard.java
+++ src/main/java/spring/postproject/Excetion/ExceptionBoard.java
@@ -15,6 +15,8 @@ public enum ExceptionBoard {
 
     NOT_FOUND_MEMBER(new NotFoundException("사용자를 찾을 수 없습니다.",400)),
     NOT_FOUND_POST(new NotFoundException("게시글을 찾을 수 없습니다.",400)),
+    NOT_FOUND_COMMENT(new NotFoundException("댓글을 찾을 수 없습니다.",400)),
+    FORBIDDEN(new BadRequestException("잘못된 요청입니다.",403)),
     INVALID_LENGTH(new BadRequestException("잘못된 길이입니다.",404)),
     INVALID_PASSWORD(new BadRequestException("잘못된 비밀번호 입니다.",404)),
     INVALID_CONTENT(new BadRequestException("잘못된 형식의 컨텐츠입니다.",404)),
---
+++ src/main/java/spring/postproject/Post/Controller/PostApiController.java
@@ -0,0 +1,44 @@
+package spring.postproject.Post.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import spring.postproject.Comment.Entitiy.Comment;
+import spring.postproject.Comment.Service.CommentService;
+import spring.postproject.Comment.dto.CommentSaveDto;
+import spring.postproject.Excetion.ExceptionBoard;
+import spring.postproject.Member.Entity.Member;
+import spring.postproject.config.Security.Annotation.CurrentMember;
+
+import javax.validation.constraints.NotBlank;
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequiredArgsConstructor
+public class PostApiController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/post/{postId}/comment")
+    public void commentSave(@RequestBody @NotBlank CommentSaveDto commentSaveDto){
+        commentService.save(commentSaveDto);
+//        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/post/{postId}/comment/{commentId}")
+    public void commentDelete(@CurrentMember Member member, @PathVariable("postId") Long postId, @PathVariable("commentId")Long commentId){
+        Comment comment = commentService.findOne(commentId);
+        Member writer = comment.getMember();
+        if(!writer.getId().equals(member.getId())){
+            throw ExceptionBoard.FORBIDDEN.getException();
+//            return new ResponseEntity(HttpStatus.FORBIDDEN);
+        }
+
+        commentService.delete(comment);
+//        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
