ee1e23e4aa9ab0a4b725fc4a1143209200170c54
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-11-15 21:14:40 +0900

    feat : Post Create, Update 하는 Dto 분리

--- src/main/java/spring/postproject/Post/Controller/PostController.java
+++ src/main/java/spring/postproject/Post/Controller/PostController.java
@@ -15,7 +15,9 @@ import spring.postproject.Comment.Service.CommentService;
 import spring.postproject.Comment.dto.CommentSaveDto;
 import spring.postproject.Member.Entity.Member;
 import spring.postproject.Post.Entity.Post;
+import spring.postproject.Post.PostDto.PostCreateDto;
 import spring.postproject.Post.PostDto.PostDto;
+import spring.postproject.Post.PostDto.PostUpdateDto;
 import spring.postproject.Post.Service.PostService;
 import spring.postproject.config.Security.Annotation.CurrentMember;
 import spring.postproject.config.Security.model.MemberAdaptor;
@@ -46,33 +48,27 @@ public class PostController {
 
     @GetMapping("/post/new")
     public String createPost(Model model) {
-        model.addAttribute("postDto", new PostDto());
+        model.addAttribute("postCreateDto", new PostCreateDto());
         return "/post/postCreate";
     }
 
     @PostMapping("/post/new")
 //    public String create(PostDto postDto, @SessionAttribute(name = "login",required = false) Member member){
-    public String create(@Valid PostDto postDto, BindingResult result, Model model, Long memberId, @CurrentMember Member member , List<MultipartFile> files) throws IOException {
+    public String create(@Valid PostCreateDto postCreateDto, BindingResult result, Model model, @CurrentMember Member member , List<MultipartFile> files) throws IOException {
         if(result.hasErrors()){
             return "post/postCreate";
         }
-        log.info("title : " + postDto.getTitle() + " content : " + postDto.getContent());
 
 
+        postCreateDto.setFiles(files);
+        postCreateDto.setMemberId(member.getId());
+//        Post post = Post.builder()
+//                .title(postDto.getTitle())
+//                .content(postDto.getContent())
+//                .build();
 
+        postService.create(postCreateDto);
 
-        Post post = Post.builder()
-                .title(postDto.getTitle())
-                .content(postDto.getContent())
-                .build();
-
-        postService.create(post, member.getId(), files);
-        log.info("post new ");
-
-        log.info("post : " + post);
-        log.info("member id : " + memberId);
-        log.info("post : " + post);
-        log.info("post id : " + post.getId() + " post title : " + post.getTitle() + " post content : " + post.getContent());
         return "redirect:/";
     }
 
@@ -89,26 +85,22 @@ public class PostController {
     @GetMapping("/post/{postId}/update")
     public String updateForm(@PathVariable("postId") Long postId, Model model) {
         Post post = postService.findOne(postId);
-        log.info("post update get");
-        log.info("post : " + post);
-        log.info("post id : " + post.getId() + " post title : " + post.getTitle() + " post content : " + post.getContent());
-        PostDto postDto = new PostDto();
-        postDto.toDto(post);
-        model.addAttribute("postDto", postDto);
+        PostUpdateDto postUpdateDto = new PostUpdateDto();
+        postUpdateDto.toDto(post);
+        model.addAttribute("postUpdateDto", postUpdateDto);
         model.addAttribute("postId", postId);
         model.addAttribute("fileList",post.getFileList());
         return "post/postUpdate";
     }
 
     @PostMapping("/post/{postId}/update")
-    public String update(@PathVariable("postId") Long postId, @Valid PostDto postDto, BindingResult result, Model model,List<MultipartFile> files) throws IOException {
+    public String update(@PathVariable("postId") Long postId, @Valid PostUpdateDto postUpdateDto, BindingResult result,  Model model,  List<MultipartFile> files) throws IOException {
         if(result.hasErrors()){
             model.addAttribute("postId",postId);
             return "post/postUpdate";
         }
-        Post post = postService.update(postId, postDto,files);
-        log.info("post : " + post);
-        log.info("post id : " + post.getId() + " post title : " + post.getTitle() + " post content : " + post.getContent());
+        postUpdateDto.setFiles(files);
+        postService.update(postId, postUpdateDto);
         return "redirect:/";
     }
 
