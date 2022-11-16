91b72524de590ab7149ece033736a563bab89732
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-11-17 00:31:19 +0900

    feat : add MemberInfoDto field

--- src/main/java/spring/postproject/Comment/dto/CommentInfoDto.java
+++ src/main/java/spring/postproject/Comment/dto/CommentInfoDto.java
@@ -2,16 +2,19 @@ package spring.postproject.Comment.dto;
 
 import lombok.*;
 import spring.postproject.Comment.Entitiy.Comment;
+import spring.postproject.Member.dto.MemberInfoDto;
 
 
 @Getter
 public class CommentInfoDto {
     private Long id;
     private String content;
+    private MemberInfoDto memberInfoDto;
 
     @Builder
     public CommentInfoDto(Comment comment){
         this.id = comment.getId();
         this.content = comment.getContent();
+        this.memberInfoDto = MemberInfoDto.builder().member(comment.getMember()).build();
     }
 }
