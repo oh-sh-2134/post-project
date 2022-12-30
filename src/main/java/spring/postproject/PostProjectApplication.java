5e52bdf047ca6aeee2b286e42db38c4b1e611a0a
sh-oh-2134 <62948776+oh-sh-2134@users.noreply.github.com>
2022-12-30 12:28:50 +0900

    add : viewCount update Scheduler

--- src/main/java/spring/postproject/PostProjectApplication.java
+++ src/main/java/spring/postproject/PostProjectApplication.java
@@ -4,15 +4,15 @@ package spring.postproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class PostProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostProjectApplication.class, args);
-
 	}
-
 }
---
+++ src/main/java/spring/postproject/config/Redis/RedisSchedule.java
@@ -0,0 +1,38 @@
+package spring.postproject.config.Redis;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import spring.postproject.Post.Repository.PostRepositorySupport;
+import spring.postproject.Post.Service.PostService;
+
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Component
+public class RedisSchedule {
+
+    private final RedisTemplate<String,Object> redisTemplate;
+    private final PostService postService;
+    @Scheduled(cron = "0 0/3 * * * ?")
+    public void deleteViewCntCacheFromRedis() {
+        RedisTemplate<String, Object> rt = redisTemplate;
+        Set<String> redisKeys = redisTemplate.keys("productViewCnt*");
+        if(redisKeys == null) return;
+        redisKeys = redisKeys.stream().filter(Objects::nonNull).collect(Collectors.toSet());
+        for (String key : redisKeys) {
+            Object o = rt.opsForValue().get(key);
+            if(o == null) continue;
+            Long postId = Long.parseLong(key.split("::")[1]);
+            Long viewCnt = Long.parseLong((String)o);
+            postService.addViewCntFromRedis(postId,viewCnt);
+            rt.delete(key);
+            rt.delete("postCnt::"+postId);
+        }
+    }
+}
